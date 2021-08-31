71fa60807c90fe309dab5d544fb007ed2529cf38
herdinugroho <herdi.nugroho1912@gmail.com>
2021-08-31 23:23:30 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+
+ function clickme(smallImg) {
+
+            var fullImg = document.getElementById("imagebox");
+            fullImg.src = smallImg.src;
+
+        }
+
